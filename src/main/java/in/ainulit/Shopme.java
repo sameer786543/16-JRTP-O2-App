c2ebdfa2e3a38d58a8543ef8a9e9759f732570b5
Ainul <aliainul9568@gmail.com>
2021-07-20 14:01:39 +0530

    Modified main method

--- src/main/java/in/ainulit/Shopme.java
+++ src/main/java/in/ainulit/Shopme.java
@@ -3,6 +3,10 @@ package in.ainulit;
 public class Shopme {
 	public static void main(String[] args) {
 		if(System.out.printf("Wlcome To Our Website")==null);
+		System.out.println("Hello Modified Code Is there");
+		int a=5;
+		int b=10;
+		
 	}
 
 }
